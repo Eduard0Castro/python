896b926d4a098d8d3ccf65337f0d9b87715c6664
Eduardo Castro <americogomes1@outlook.com>
2023-08-08 19:48:10 -0300

    Testes...

--- Python scripts/Trivial/media_2.py
+++ Python scripts/Trivial/media_2.py
@@ -3,10 +3,12 @@ import time
 soma = 0
 notas = int(input('Quantas notas serão aplicadas? '))
 nota = 0
+mult = 1
 for i in range (1, notas + 1):
     nota = float(input('Digite a nota {}: ' .format(i)))
     soma += nota
-
+    
 media = (soma) / notas
 print('A media das notas é: {:.2f}' .format(media))
+
 
