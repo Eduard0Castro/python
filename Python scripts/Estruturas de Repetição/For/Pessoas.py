7bb73da105f8efe7e37754591ee7fdb36dc5d821
Eduardo Castro <americogomes1@outlook.com>
2023-08-08 18:23:57 -0300

    Atualização

--- Python scripts/Estruturas de Repetição/For/Pessoas.py
+++ Python scripts/Estruturas de Repetição/For/Pessoas.py
@@ -5,15 +5,16 @@ heil = 0
 cont = 0
 maior = 0
 for p in range(0,4):
+    print('-', f'{p+1}ª Pessoa: -\n', )
     nome = str(input('Digite seu nome: ')).capitalize().strip()
     idade = int(input('Digite sua idade: '))
-    sexo = str(input('Digite seu sexo: ')).capitalize().strip()
+    sexo = str(input('Digite seu sexo[M/F]: ')).capitalize().strip()
   
     heil += idade
-    if sexo == 'Fem' and idade < 20:
+    if sexo == 'F' and idade < 20:
         cont +=1
 
-    if sexo == 'Masc':
+    if sexo == 'M':
         if idade > maior:
             maior = idade
             velho = nome
