b9c8fa4642489a902147d8b4ce9c0864579e3e4a
Eduardo Castro <americogomes1@outlook.com>
2023-08-10 10:56:07 -0300

    While

---
+++ Python scripts/Estruturas de Repetição/While/Soma de numeros digitados.py
@@ -0,0 +1,12 @@
+cont = -1
+n = 0
+soma = 0
+while n != 999:
+    soma +=n
+    cont +=1
+    n = int(input('Digite um numero: '))
+    
+    
+
+print(f'A quantidade de numeros digitados foi {cont}')
+print(f'A soma dos números é {soma}')
