54b308e474ee7a15b5cc1e02935c6ed4de395539
Eduardo Castro <americogomes1@outlook.com>
2023-08-13 20:46:23 -0300

    Final While

---
+++ Python scripts/Estruturas de Repetição/While/Numeros e While.py
@@ -0,0 +1,28 @@
+count = 0
+media = 0
+res = str
+heil = 0
+maior = 0
+menor = 0
+
+while res != 'N':
+    
+    n = float(input('Digite um numero: '))
+    count +=1
+    heil += n
+    
+    #para o primeiro valor digitado, menor recebe n
+    if count == 1:
+        menor = n 
+
+    if maior < n:
+        maior = n
+
+    if menor > n :
+        menor = n 
+
+    if count%5 == 0:
+        res = str(input('Deseja continuar [S/N]: \n')).upper()
+    
+media = heil/count
+print(f'O maior numero digitado foi {maior}\nO menor foi {menor}\nQuantidade de numero digitados: {count}\nMédia: {media}')
